f7a7afc8cab300459a9baf0352206b3109ead595
AKS <ks.ang.coding@gmail.com>
2023-06-10 17:49:56 +0800

    added supabase google auth

--- web-app/src/pages/LoginPage.js
+++ web-app/src/pages/LoginPage.js
@@ -1,29 +1,38 @@
 import React from "react";
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { useContext } from "react";
-// import clientID from .env
 import { ApplicationContext } from "../contexts/ApplicationContext";
 
-const { REACT_APP_GOOGLE_AUTH_CLIENT_ID } = process.env;
 
 function LoginPage() {
-  const responseMessage = (response) => {
-    console.log(response);
-  };
-  const errorMessage = (error) => {
-      console.log(error);
-  };
 
-  const { user, setUser, isAuthenticated, setIsAuthenticated } = useContext(ApplicationContext)
+  const { user, supabaseClient } = useContext(ApplicationContext)
+
+  const handleLogin = async (event) => {
+
+    const { data, error } = await supabaseClient.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "https://localhost:3000/dashboard",
+      },
+    });
+    console.log(data)
+    console.log(error)
+  }
+
     
   return (
     <div>
       <h1>Login Page</h1>
       <p>welcome back:{user}</p>
       <div className="max-w-sm">
-        <GoogleOAuthProvider clientId={REACT_APP_GOOGLE_AUTH_CLIENT_ID}>
-          <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-        </GoogleOAuthProvider>
+        {/* create a login button */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleLogin}
+        >
+          Login with Google
+        </button>
+
       </div>
     </div>
   );

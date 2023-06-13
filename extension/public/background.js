e645bc5fd80e0b6316dbb68f2539a991c35f8b6f
choonwei0214 <92849561+choonwei0214@users.noreply.github.com>
2023-06-13 19:10:03 +0800

    Updated background and content script (not yet done)

--- extension/public/background.js
+++ extension/public/background.js
@@ -1,6 +1,41 @@
-// Listen for messages from the content script
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log(message.greeting); // Outputs: "Hello from content script!"
-  });
+// // Listen for messages from the content script
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//     console.log(message.greeting); // Outputs: "Hello from content script!"
+//   });
 
-  
+//   // background.js
+
+// // Event listener for tab creation
+// chrome.tabs.onCreated.addListener(function(tab) {
+//   console.log('New tab created:', tab.url);
+//   // Perform your desired actions here when a new tab is created
+//   // You can include your web scraping logic or any other functionality
+// });
+
+// // Event listener for tab update (e.g., page refresh)
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//   if (changeInfo.status === 'loading') {
+//     console.log('Tab updated:', tab.url);
+//     // Perform your desired actions here when a page is refreshed
+//     // You can include your web scraping logic or any other functionality
+//   }
+// });
+// background.js
+
+// Event listener for tab update (e.g., page refresh)
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === 'loading') {
+    console.log("loading")
+    chrome.tabs.sendMessage(tabId, { action: 'scrapePage' });
+  }
+});
+
+// Listen for the scraped content sent from the content script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.content) {
+    console.log('Scraped content:', request.content);
+    // Process the scraped content as per your requirement
+    // You can also send it to other components of the extension if needed
+  }
+});
+
--- extension/public/contentScript.js
+++ extension/public/contentScript.js
@@ -1,6 +1,17 @@
-// Send a message to the background script
-chrome.runtime.sendMessage({ greeting: "Hello from content script!" });
+console.log("content")
+// Function to scrape the page content
+function scrapePageContent() {
+  // Extract the necessary data from the DOM
+  // Replace this code with your specific scraping logic
+  const pageContent = document.documentElement.innerHTML;
 
-chrome.storage.local.set({ "lol": "ok" }).then(() => {
-    console.log("Value is set");
-  });
+  // Send the scraped content back to the background script
+  chrome.runtime.sendMessage({ content: pageContent });
+}
+
+// Listen for a message from the background script to start scraping
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'scrapePage') {
+    scrapePageContent();
+  }
+});

20bed97ada0e6a8d2dbbc0d349452f5c20ab16c8
SHI TINGXIAO <stx8@outlook.com>
2023-06-14 00:40:10 +0800

    update home page layout

--- extension/dist/contentScript.js
+++ extension/dist/contentScript.js
@@ -1,1 +1,17 @@
-function scrapePageContent(){const e=document.documentElement.innerHTML;chrome.runtime.sendMessage({content:e})}console.log("content"),chrome.runtime.onMessage.addListener((function(e,n,t){"scrapePage"===e.action&&scrapePageContent()}));
+console.log("content")
+// Function to scrape the page content
+function scrapePageContent() {
+  // Extract the necessary data from the DOM
+  // Replace this code with your specific scraping logic
+  const pageContent = document.documentElement.innerHTML;
+
+  // Send the scraped content back to the background script
+  chrome.runtime.sendMessage({ content: pageContent });
+}
+
+// Listen for a message from the background script to start scraping
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'scrapePage') {
+    scrapePageContent();
+  }
+});

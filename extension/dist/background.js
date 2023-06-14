aec7b870e057a064dc59afa1f47f4a32be373b69
SHI TINGXIAO <stx8@outlook.com>
2023-06-15 01:22:57 +0800

    update text extraction script

--- extension/dist/background.js
+++ extension/dist/background.js
@@ -10,18 +10,31 @@
 //   }
 // });
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.localStorageData && message.website == "mindful-beans.netlify.app") {
-    const localStorageData = message.localStorageData;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "logInput") {
+        console.log("Input value:", request.inputValue);
+    } else if (request.content) {
+        console.log("Scraped content:", request.content);
+    } else if (
+        request.localStorageData &&
+        request.website == "mindful-beans.netlify.app"
+    ) {
+        const localStorageData = request.localStorageData;
 
-    chrome.storage.local.set(localStorageData, function() {
-      console.log('LocalStorage data copied and saved to extension storage.');
+        chrome.storage.local.set(localStorageData, function () {
+            console.log(
+                "LocalStorage data copied and saved to extension storage."
+            );
 
-      chrome.storage.local.get("sb-uippnkhijtqmwnwnnypz-auth-token", function(result) {
-        console.log('Retrieved data from extension storage:', result);
-      });
-    });
-  }
+            chrome.storage.local.get(
+                "sb-uippnkhijtqmwnwnnypz-auth-token",
+                function (result) {
+                    console.log(
+                        "Retrieved data from extension storage:",
+                        result
+                    );
+                }
+            );
+        });
+    }
 });
-
-// chrome.storage.local.clear()
--- extension/public/contentScript.js
+++ extension/public/contentScript.js
@@ -5,6 +5,9 @@ const observer = new MutationObserver(function (mutationsList) {
       // Get the updated page content
       const pageContent = document.documentElement.innerText;
       chrome.runtime.sendMessage({ content: pageContent });
+      
+      // Capture and log the user's inputs
+      captureUserInputs();
     }
   }
 });
@@ -19,7 +22,35 @@ function retrieveLocalStorageData() {
     } else {
       setTimeout(retrieveLocalStorageData, 500);
     }
-  };
+  }
 };
 
+// Function to capture and log the user's inputs
+function captureUserInputs() {
+  // Get all input and textarea elements on the page
+  const inputs = document.querySelectorAll('input, textarea');
+
+  // Iterate over each input and textarea element
+  for (let i = 0; i < inputs.length; i++) {
+    // Check if we've already added an event listener to this element
+    if (inputs[i].dataset.listening !== 'true') {
+      // Add an event listener to capture the input's value every time it is changed
+      inputs[i].addEventListener('input', function(event) {
+        // Get the value of the input element
+        const inputValue = event.target.value;
+
+        // Send a message to the background script to log the input value
+        chrome.runtime.sendMessage({ action: "logInput", inputValue: inputValue });
+      });
+
+      // Mark the input as having an event listener
+      inputs[i].dataset.listening = 'true';
+    }
+  }
+}
+
+// Call the captureUserInputs function at the start to handle any already existing inputs
+captureUserInputs();
+
+// Call the retrieveLocalStorageData function
 retrieveLocalStorageData();

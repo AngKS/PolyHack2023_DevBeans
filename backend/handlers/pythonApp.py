27cd19b5a891fb072530f334d68dcae04c3a6e8b
AKS <ks.ang.coding@gmail.com>
2023-06-17 20:29:24 +0800

    template backend

---
+++ backend/handlers/pythonApp.py
@@ -0,0 +1,8 @@
+
+
+def mainHandler(event, context):
+    a = 1
+    b = 2
+    print(a + b)
+    
+    return "Hello World! from Python"
--- web-app/src/utils/databaseUtils.js
+++ web-app/src/utils/databaseUtils.js
@@ -4,6 +4,46 @@ const client = supabase.createClient(
   process.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
+
+const getBrowsingHistory = async (user_id) => {
+  const TABLE_NAME = "Browsing Activities Table"
+
+  let { data, error } = await client
+    .from(TABLE_NAME)
+    .select("*")
+    .eq("user_id", user_id);
+  if (error) {
+    return {
+      statusCode: 400,
+      body: {
+        data: null,
+        payload: null,
+      },
+    }
+  }
+  else {
+    if (data.length > 0) {
+      return {
+        statusCode: 200,
+        body: {
+          data: data,
+          payload: JSON.stringify(data),
+        },
+      }
+    }
+    else {
+      return {
+        statusCode: 404,
+        body: {
+          data: null,
+          payload: null,
+        },
+      }
+    }
+
+  }
+}
+
 // read database
 const readFullDatabase = async (tableName) => {
   console.log(tableName);

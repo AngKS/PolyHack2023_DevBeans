badab28504c50b94468e10db4c7d226784930572
choonwei0214 <92849561+choonwei0214@users.noreply.github.com>
2023-06-18 21:38:07 +0800

    Updated input purification toggle

--- extension/public/contentScript.js
+++ extension/public/contentScript.js
@@ -326,27 +326,25 @@ function captureUserInputs() {
 
 
 // Call the captureUserInputs function at the start to handle any already existing inputs
-// chrome.storage.local.get("inputPurification", function (result) {
-//     if (result.inputPurification) {
-//         document.addEventListener("keydown", captureUserInputs);
-//     }
-// });
+chrome.storage.local.get("inputPurification", function (result) {
+    if (result.inputPurification) {
+        document.addEventListener("keydown", captureUserInputs);
+    }
+});
 
-// chrome.storage.onChanged.addListener(function (changes) {
-//     if (changes.inputPurification && !changes.inputPurification.newValue) {
-//         // If inputPurification becomes false, remove the event listener
-//         console.log("removed");
-//         document.removeEventListener("keydown", captureUserInputs);
-//     } else if (
-//         !changes.inputPurification &&
-//         changes.inputPurification.newValue
-//     ) {
-//         // If inputPurification becomes true, add the event listener
-//         document.addEventListener("keydown", captureUserInputs);
-//     }
-// });
+chrome.storage.onChanged.addListener(function (changes) {
+    if (changes.inputPurification && !changes.inputPurification.newValue) {
+        // If inputPurification becomes false, remove the event listener
+        document.removeEventListener("keydown", captureUserInputs);
+    } else if (
+        !changes.inputPurification &&
+        changes.inputPurification.newValue
+    ) {
+        // If inputPurification becomes true, add the event listener
+        document.addEventListener("keydown", captureUserInputs);
+    }
+});
 
-document.addEventListener("keydown", captureUserInputs);
 
 // To retrieve token from local storage
 function retrieveLocalStorageData() {
